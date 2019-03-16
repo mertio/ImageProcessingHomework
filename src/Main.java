38e7c21973cf49738426c7edb1147800e7449055
mertio <mert7000@gmail.com>
2019-03-16 12:52:45 +0300

    making progress on hw2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,33 @@ public class Main {
 
         MyImage myImage = new MyImage("res/sample.jpeg");
 
-
+        // NEIGHBOR RESIZE
         // scale to HD with neighbor resize
         //myImage.neighborResize(1920,1080);
 
+        // BILINEAR RESIZE
         // scale to HD with bilinear resize
-        myImage.bilinearResize(800, 600);
+        //myImage.bilinearResize(1920, 1080);
+
+
+        // Applies box filter to sample, image shrinks and gets black frame because
+        // edges and resize aren't handled
+        //myImage.applyBoxFilter(9);
+
+
+        // Applies highpass filter
+        // can use 3 types of filters as param x = 0 or 1 or 2
+        //myImage.applyHighpassFilter(0);
+
+
+        // Applies sharpen filter
+        //myImage.applySharpenFilter();
+
+        // Applies Emboss filter
+        myImage.applyEmbossFilter();
+
+
+
 
 
 
--- src/MyImage.java
+++ src/MyImage.java
@@ -143,6 +143,7 @@ public class MyImage {
 
         int pixel  = img.getRGB(x,y);
 
+        int alpha = (pixel >> 24) & 0xff;
         int red = (pixel >> 16) & 0xff;
         int green = (pixel >> 8) & 0xff;
         int blue = (pixel) & 0xff;
@@ -160,7 +161,7 @@ public class MyImage {
 
         }
 
-        int sum = (red << 16) + (green << 8) + blue;
+        int sum = (alpha << 24) | (red << 16) | (green << 8) | blue;
         img.setRGB(x,y,sum);
 
 
@@ -275,6 +276,61 @@ public class MyImage {
 
     }
 
+    private BufferedImage bilinearResize(BufferedImage imageFile, int newWidth, int newHeight) {
+
+        int oldWidth = imageFile.getWidth();
+        int oldHeight = imageFile.getHeight();
+
+        double xRatio = (oldWidth - 1) / (double) newWidth;
+        double yRatio = (oldHeight - 1) / (double) newHeight;
+
+        BufferedImage img = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+
+        int px,py;
+        double xdiff, ydiff;
+        int A, B, C, D;
+        for(int x = 0; x < newWidth; x++) {
+            for(int y = 0; y < newHeight; y++) {
+
+                px = (int)(x*xRatio);
+                py = (int)(y*yRatio);
+
+                xdiff = x*xRatio - px;
+                ydiff = y*yRatio - py;
+
+                int r,g,b;
+
+                A = getPixel((int)px, (int)py, Channel.RED);
+                B = getPixel((int)px + 1, (int)py, Channel.RED);
+                C = getPixel((int)px, (int)py + 1, Channel.RED);
+                D = getPixel((int)px + 1, (int)py + 1, Channel.RED);
+
+                r = (int)((A * (1 - xdiff) * (1 - ydiff)) + (B * xdiff * (1 - ydiff)) + (C * ydiff * (1 - xdiff)) + (D * xdiff * ydiff));
+
+                A = getPixel( (int)px, (int)py, Channel.GREEN);
+                B = getPixel( (int)px + 1, (int)py, Channel.GREEN);
+                C = getPixel( (int)px, (int)py + 1, Channel.GREEN);
+                D = getPixel( (int)px + 1, (int)py + 1, Channel.GREEN);
+
+                g = (int)((A * (1 - xdiff) * (1 - ydiff)) + (B * xdiff * (1 - ydiff)) + (C * ydiff * (1 - xdiff)) + (D * xdiff * ydiff));
+
+                A = getPixel( (int)px, (int)py, Channel.BLUE);
+                B = getPixel( (int)px + 1, (int)py, Channel.BLUE);
+                C = getPixel( (int)px, (int)py + 1, Channel.BLUE);
+                D = getPixel( (int)px + 1, (int)py + 1, Channel.BLUE);
+
+                b = (int)(A * (1 - xdiff) * (1 - ydiff) + B * xdiff * (1 - ydiff) + C * ydiff * (1 - xdiff) + D * xdiff * ydiff);
+
+
+                img.setRGB(x,y,new Color(r,b,g).getRGB());
+
+
+            }
+        }
+        return img;
+    }
+
+
     public void bilinearResize(int newWidth, int newHeight) {
 
         int oldWidth = imageFile.getWidth();
@@ -320,9 +376,8 @@ public class MyImage {
 
                 b = (int)(A * (1 - xdiff) * (1 - ydiff) + B * xdiff * (1 - ydiff) + C * ydiff * (1 - xdiff) + D * xdiff * ydiff);
 
-                setPixelNoAlpha(img, x, y, Channel.RED, r);
-                setPixelNoAlpha(img, x, y, Channel.GREEN, g);
-                setPixelNoAlpha(img, x, y, Channel.BLUE, b);
+
+                img.setRGB(x,y,new Color(r,b,g).getRGB());
 
 
             }
@@ -330,6 +385,9 @@ public class MyImage {
 
         createImageFile(img,"res/bilinear_resize.jpg");
     }
+
+
+
 
     public void neighborResize(int newWidth, int newHeight) {
 
@@ -417,6 +475,202 @@ public class MyImage {
         createImageFile(img,"res/cool_reddish_effect.jpg");
     }
 
+
+
+
+
+
+    public void applyBoxFilter(int w) {
+        double[][] filter = getBoxFilter(w);
+        BufferedImage img = convolve(filter);
+        createImageFile(img,"res/applied_box_filter.jpg");
+    }
+
+    public void applyHighpassFilter(int x) {
+        double[][] filter = getHighpassFilter(x);
+        BufferedImage img = convolve(filter);
+        createImageFile(img,"res/applied_highpass_filter.jpg");
+    }
+
+    public void applySharpenFilter() {
+        double[][] filter = getSharpenFilter();
+        BufferedImage img = convolve(filter);
+        createImageFile(img,"res/applied_sharpen_filter.jpg");
+    }
+
+    public void applyEmbossFilter() {
+        double[][] filter = getEmbossFilter();
+        BufferedImage img = convolve(filter);
+        createImageFile(img,"res/applied_emboss_filter.jpg");
+    }
+
+
+    private double[][] getBoxFilter(int w) {
+
+
+        double[][] boxFilter = new double[w][w];
+
+        for (int i = 0; i < boxFilter.length; i++) {
+
+            for(int j = 0; j < boxFilter.length; j++) {
+
+                boxFilter[i][j] = 1.0/(boxFilter.length*boxFilter.length);
+
+            }
+
+        }
+        return boxFilter;
+    }
+    //TODO
+    private double[][] getHighpassFilter(int x) {
+
+
+        if(x == 0) {
+            double[][] highpassFilter = {
+                    {-1, -1, -1},
+                    {-1, +8, -1},
+                    {-1, -1, -1}
+            };
+            return highpassFilter;
+        }
+
+        else if(x == 1) {
+            double[][] highpassFilter = {
+                    {0, -1, 0},
+                    {-1, +4, -1},
+                    {0, -1, 0}
+            };
+            return highpassFilter;
+        }
+
+        else {
+            double[][] highpassFilter = {
+                    {1, -2, 1},
+                    {-2, +4, -2},
+                    {1, -2, 1}
+            };
+            return highpassFilter;
+        }
+
+    }
+
+    //TODO
+    private double[][] getSharpenFilter() {
+
+
+        double[][] sharpenFilter = {
+                {-1, -1, -1},
+                {-1, +9, -1},
+                {-1, -1, -1}
+        };
+
+
+        return sharpenFilter;
+    }
+
+    //TODO
+    private double[][] getEmbossFilter() {
+
+
+        double[][] embossFilter = {
+                {-2, 0, 0},
+                {0, 1, 0},
+                {0, 0, 2}
+        };
+
+
+
+
+        return embossFilter;
+    }
+
+    //TODO
+    private int[][] getGaussianFilter(float sigma) {
+
+
+
+        return null;
+    }
+
+    private BufferedImage correlate(double[][] filter) {
+
+        BufferedImage img = new BufferedImage(getImageFile().getWidth(), getImageFile().getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        double linearSumRed = 0;
+        double linearSumBlue = 0;
+        double linearSumGreen = 0;
+
+        for (int i = 0; i < imageFile.getWidth() - filter.length; i++) {
+            for(int j = 0; j < imageFile.getHeight() - filter.length; j++) {
+
+                for(int k = 0; k < filter.length; k++) {
+                    for(int l = 0; l < filter.length; l++) {
+
+                        linearSumRed += ((double)getPixel(i + k,j + l, Channel.RED)) * filter[k][l];
+                        linearSumBlue += ((double)getPixel(i + k,j + l, Channel.BLUE)) * filter[k][l];
+                        linearSumGreen += ((double)getPixel(i + k,j + l, Channel.GREEN)) * filter[k][l];
+
+
+                    }
+                }
+
+                //setPixelNoAlpha(img, i + filter.length/2, j + filter.length/2, Channel.RED, linearSumRed/(filter.length*filter.length));
+                //setPixelNoAlpha(img, i + filter.length/2, j + filter.length/2, Channel.BLUE, linearSumBlue/(filter.length*filter.length));
+                //setPixelNoAlpha(img, i + filter.length/2, j + filter.length/2, Channel.GREEN, linearSumGreen/(filter.length*filter.length));
+
+                if(linearSumRed > 255) {
+                    linearSumRed = 255;
+                }
+                if(linearSumGreen > 255) {
+                    linearSumGreen = 255;
+                }
+                if(linearSumBlue > 255) {
+                    linearSumBlue = 255;
+                }
+                if(linearSumRed < 0) {
+                    linearSumRed = 0;
+                }
+                if(linearSumGreen < 0) {
+                    linearSumGreen = 0;
+                }
+                if(linearSumBlue < 0) {
+                    linearSumBlue = 0;
+                }
+
+                img.setRGB(i + filter.length/2, j + filter.length/2, new Color((int)linearSumRed,(int)linearSumBlue,(int)linearSumGreen).getRGB());
+
+                linearSumRed = 0;
+                linearSumBlue = 0;
+                linearSumGreen = 0;
+            }
+        }
+
+        //img = bilinearResize(img, getImageFile().getWidth(), getImageFile().getHeight());
+
+        return img;
+    }
+
+    private BufferedImage convolve(double[][] filter) {
+        filter = rotateFilter180(filter);
+        return correlate(filter);
+
+    }
+
+
+    private double[][] rotateFilter180(double[][] filter) {
+
+        int N = filter.length;
+
+        for (int i = 0; i < N / 2; i++) {
+            for(int j = 0; j < N; j++) {
+                double temp = filter[i][j];
+                filter[i][j] = filter[N - i - 1][N - j - 1];
+                filter[N - i - 1][N - j - 1] = temp;
+            }
+        }
+
+        return filter;
+    }
 
 
 
